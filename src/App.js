6194cffe59ed6fe86aa49d3be271fe10287a68e9
BerMu147 <berin.mujcinovic.98@gmail.com>
2023-11-20 03:46:49 +0100

    Modify the UI

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, TextField, Typography } from '@material-ui/core';
+import ImageIcon from '@material-ui/icons/Image';
 
 function App() {
-  const [data, setData] = useState('');
+ const [imageUrl, setImageUrl] = useState('');
 
-  useEffect(() => {
-    (async function () {
-      const { text } = await( await fetch(`/api/message`)).json();
-      setData(text);
-    })();
-  });
+ const handleSubmit = () => {
+    // Perform any necessary actions here
+    console.log('Image URL:', imageUrl);
+ };
 
-  return <div>{data}</div>;
+ return (
+    <div className="App">
+      <Typography variant="h4" align="center" gutterBottom>
+        Image Viewer
+      </Typography>
+      <TextField
+        id="outlined-full-width"
+        label="Image URL"
+        style={{ margin: 8 }}
+        placeholder="Enter image URL"
+        fullWidth
+        margin="normal"
+        variant="outlined"
+        value={imageUrl}
+        onChange={(e) => setImageUrl(e.target.value)}
+      />
+      <Button
+        variant="contained"
+        color="primary"
+        startIcon={<ImageIcon />}
+        style={{ margin: 8 }}
+        onClick={handleSubmit}
+      >
+        Load Image
+      </Button>
+    </div>
+ );
 }
 
 export default App;
